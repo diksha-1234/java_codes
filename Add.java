92c1da03e48601cd597e859e704de8269fbe8639
diksha-1234 <diya01210@gmail.com>
2024-04-10 22:04:27 +0530

    addition

---
+++ Add.java
@@ -0,0 +1,9 @@
+//Write a java program to add two given numbers.
+public class Add {
+    public static void main(String[] args) {
+        int firstNumber=5;
+        int secondNumber=7;
+        int addTwoNumbers=firstNumber+secondNumber;
+        System.out.println(addTwoNumbers);
+    }
+}
