54542661f5fab55ec7b57608369afe969423e82b
diksha-1234 <diya01210@gmail.com>
2024-04-22 21:37:34 +0530

    pattern

---
+++ PatternFirst.java
@@ -0,0 +1,16 @@
+/*Write a java program to print pattern
+*****
+*****
+*****
+*****
+*/
+class PatternFirst {
+    public static void main(String[] args) {
+        for (int i = 1; i <=4; i++) {
+            for (int j = 1; j <=5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
